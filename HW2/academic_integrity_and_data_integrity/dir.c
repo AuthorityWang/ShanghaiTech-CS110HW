8d336557bbca0a2ed2ddac9102938fa371c13e6a
QuanWang <2453306868@qq.com>
2023-02-20 23:56:55 +0800

    task 2 and task 3

--- HW2/academic_integrity_and_data_integrity/dir.c
+++ HW2/academic_integrity_and_data_integrity/dir.c
@@ -7,24 +7,26 @@
 
 /* define a dir_add_sub function */
 static bool dir_add_sub(struct directory *dirnode, struct node *sub) {
-  /* define the dir_add_sub for convience*/
-  int i = 0;
+  /* Initialization */
+  struct node **new_subordinates = NULL;
+  /* Check for null pointer */
   if (!dirnode || !sub) {
     return false;
   }
-  /* travseral the directory and find if same name  */
-  for (i = 0; i < dirnode->size; i++) {
-    if (strcmp(dirnode->subordinates[i]->name, sub->name) == 0) {
-      return false;
-    }
-  }
-  /* if size overflow, then realloc the previous memory */
+  /* Check if the subordinates is full */
   if (dirnode->size == dirnode->capacity) {
+    /* Allocate memory */
+    new_subordinates = calloc(dirnode->capacity * 2, sizeof(struct node));
+    /* Copy the subordinates */
+    memcpy(new_subordinates, dirnode->subordinates,
+           dirnode->capacity * sizeof(struct node));
+    /* Release the old subordinates */
+    free(dirnode->subordinates);
+    /* Update the subordinates */
+    dirnode->subordinates = new_subordinates;
     dirnode->capacity *= 2;
-    dirnode->subordinates = realloc(dirnode->subordinates,
-                                    dirnode->capacity * sizeof(struct node *));
   }
-  /* add the new sub to the subordinates */
+  /* Add the subordinates */
   dirnode->subordinates[dirnode->size++] = sub;
   return true;
 }
@@ -70,7 +72,6 @@ void dir_release(struct directory *dir) {
 struct node *dir_find_node(const struct directory *dir, const char *name) {
   /* YOUR CODE HERE */
   /* check the null ptr*/
-  int i = 0;
   if (!dir || !name) {
     return NULL;
   }
@@ -78,7 +79,7 @@ struct node *dir_find_node(const struct directory *dir, const char *name) {
     return NULL;
   }
   /* travseral the subordinates and find the name */
-  for (i = 0; i < dir->size; i++) {
+  for (int i = 0; i < dir->size; i++) {
     if (strcmp(dir->subordinates[i]->name, name) == 0) {
       return dir->subordinates[i];
     }
@@ -89,78 +90,43 @@ struct node *dir_find_node(const struct directory *dir, const char *name) {
 
 bool dir_add_file(struct directory *dir, int type, char *name) {
   /* YOUR CODE HERE */
-  struct file *file = file_new(type, name);
-  struct node *node = node_new(false, name, file);
-  /* deal with null ptr */
-  if (!file) {
-    file_release(file);
-    return false;
-  }
-  /* deal with null ptr */
-  if (!node) {
-    node_release(node);
+  if (dir_find_node(dir, name)) {
     return false;
   }
   /* deal with null ptr */
   if (!dir || !name) {
-    file_release(file);
-    node_release(node);
     return false;
   }
-  /* deal with same name */
-  if (dir_find_node(dir, name)) {
-    file_release(file);
-    node_release(node);
-    return false;
-  }
+  /* init subdir and node*/
+  struct file *subfile = file_new(type, name);
+  struct node *node = node_new(false, name, subfile);
   /* deal with possible fail case */
-  if (!dir_add_sub(dir, node)) {
-    node_release(node);
-    return false;
-  }
+  dir_add_sub(dir, node);
   /* default return true */
   return true;
 }
 
 bool dir_add_subdir(struct directory *dir, char *name) {
   /* YOUR CODE HERE */
-  struct directory *subdir = dir_new(name);
-  struct node *node = node_new(true, name, subdir);
-  /* deal with null ptr */
-  if (!subdir) {
-    dir_release(subdir);
-    return false;
-  }
-  /* deal with null ptr */
-  if (!node) {
-    node_release(node);
+  /* deal with same name */
+  if (dir_find_node(dir, name)) {
     return false;
   }
   /* deal with null ptr */
   if (!dir || !name) {
-    dir_release(subdir);
-    node_release(node);
     return false;
   }
-  /* deal with same name */
-  if (dir_find_node(dir, name)) {
-    dir_release(subdir);
-    node_release(node);
-    return false;
-  }
+  /* init subdir and node*/
+  struct directory *subdir = dir_new(name);
+  struct node *node = node_new(true, name, subdir);
   /* deal with possible fail case */
-  if (!dir_add_sub(dir, node)) {
-    node_release(node);
-    return false;
-  }
+  dir_add_sub(dir, node);
   /* default return true */
   return true;
 }
 
 bool dir_delete(struct directory *dir, const char *name) {
   /* YOUR CODE HERE */
-  int i = 0;
-  int j = 0;
   /* deal with null ptr */
   if (!dir || !name) {
     return false;
@@ -169,11 +135,11 @@ bool dir_delete(struct directory *dir, const char *name) {
     return false;
   }
   /* travseral the subordinates and find the name */
-  for (i = 0; i < dir->size; i++) {
+  for (int i = 0; i < dir->size; i++) {
     if (strcmp(dir->subordinates[i]->name, name) == 0) {
       node_release(dir->subordinates[i]);
       /* move the subordinates */
-      for (j = i; j < dir->size - 1; j++) {
+      for (int j = i; j < dir->size - 1; j++) {
         dir->subordinates[j] = dir->subordinates[j + 1];
       }
       /* decrease the size */
