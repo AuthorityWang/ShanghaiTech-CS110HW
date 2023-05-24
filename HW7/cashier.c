d863898c120fd5975733e071d2675cc2db9d3ac3
wangph1 <wangph1@shanghaitech.edu.cn>
2023-05-24 23:19:41 +0800

    add hw7 partly right

--- HW7/cashier.c
+++ HW7/cashier.c
@@ -2,65 +2,42 @@
 #include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <math.h>
 
-// get current timestamp: monotonic increasing positive integer value
-uint64_t get_timestamp(void) {
-  // Implementation depends on the platform
-  // Return a monotonic increasing positive integer value
-  // Example implementation:
-  static uint64_t timestamp = 0;
-  return ++timestamp;
-}
-
-// read a byte from the data memory
-uint8_t mem_read(uint64_t addr) {
-  // Implementation depends on the platform
-  // Read a byte from the data memory at the given address
-  // Return the value read
-  // Example implementation:
-  uint8_t *data_memory = (uint8_t *)addr;
-  return *data_memory;
-}
-
-// write a byte into the data memory
-void mem_write(uint64_t addr, uint8_t byte) {
-  // Implementation depends on the platform
-  // Write the byte into the data memory at the given address
-  // Example implementation:
-  uint8_t *data_memory = (uint8_t *)addr;
-  *data_memory = byte;
-}
-
-void before_eviction(uint64_t set_index, struct cache_line *victim) {
-  // Implementation of eviction handling
-  // This function is called before evicting a cache line
-  // You can perform any necessary operations on the victim cache line
-  // For example, if the dirty bit is set, you can write the contents back to memory
-  // In this case, since write-back is expected, we will write back the contents if the dirty bit is set
-  if (victim->dirty) {
-    uint64_t addr = (victim->tag << (victim->cache->index_bits + victim->cache->offset_bits)) |
-                    (set_index << victim->cache->offset_bits);
-    for (uint64_t i = 0; i < victim->cache->config.line_size; i++) {
-      mem_write(addr + i, victim->data[i]);
-    }
+// calculate the log2
+int log2_base2(uint64_t x) {
+  // if zero, return -1
+  if (x == 0) {
+    return -1;
   }
+  // if not zero, return the log2
+  int ret = -1;
+  // right shift until zero
+  while (x != 0) {
+    // right shift
+    x >>= 1;
+    // increment the return value
+    ret++;
+  }
+  // return the log2
+  return ret;
 }
 
 struct cashier *cashier_init(struct cache_config config) {
   // Allocate memory for the cache structure
   struct cashier *cache = (struct cashier *)malloc(sizeof(struct cashier));
+  // Check for memory allocation failure
   if (cache == NULL) {
-    return NULL; // Memory allocation failure
+    // Memory allocation failure
+    return NULL;
   }
 
   // Set the cache simulator configuration
   cache->config = config;
 
   // Calculate the number of bits for tag, index, and offset
-  cache->tag_bits = config.address_bits - (uint64_t)log2(config.line_size) - (uint64_t)log2(config.lines);
-  cache->index_bits = (uint64_t)log2(config.lines) - (uint64_t)log2(config.ways);
-  cache->offset_bits = (uint64_t)log2(config.line_size);
+  cache->tag_bits = config.address_bits - (uint64_t)log2_base2(config.line_size) - (uint64_t)log2_base2(config.lines);
+  cache->index_bits = (uint64_t)log2_base2(config.lines) - (uint64_t)log2_base2(config.ways);
+  cache->offset_bits = (uint64_t)log2_base2(config.line_size);
   
   // Calculate the bit masks for tag, index, and offset
   cache->tag_mask = ((uint64_t)1 << cache->tag_bits) - 1;
@@ -72,47 +49,72 @@ struct cashier *cashier_init(struct cache_config config) {
 
   // Allocate memory for the cache lines
   cache->lines = (struct cache_line *)malloc(sizeof(struct cache_line) * config.lines);
+  // Check for memory allocation failure
   if (cache->lines == NULL) {
-    free(cache); // Memory allocation failure, free previously allocated memory
+    // Memory allocation failure, free previously allocated memory
+    free(cache);
+    // Memory allocation failure
     return NULL;
   }
 
   // Initialize the cache lines
   for (uint64_t i = 0; i < config.lines; i++) {
+    // Initialize the cache line
     cache->lines[i].valid = false;
+    // Initialize the cache line
     cache->lines[i].dirty = false;
+    // Initialize the cache line
     cache->lines[i].tag = 0;
+    // Initialize the cache line
     cache->lines[i].last_access = 0;
+    // Allocate memory for the cache line data
     cache->lines[i].data = (uint8_t *)malloc(sizeof(uint8_t) * config.line_size);
+    // Check for memory allocation failure
     if (cache->lines[i].data == NULL) {
+      // Memory allocation failure, free previously allocated data memory
       for (uint64_t j = 0; j < i; j++) {
-        free(cache->lines[j].data); // Memory allocation failure, free previously allocated data memory
+        // Memory allocation failure, free previously allocated data memory
+        free(cache->lines[j].data);
       }
-      free(cache->lines); // Memory allocation failure, free previously allocated cache lines
-      free(cache); // Memory allocation failure, free previously allocated cache structure
+      // Memory allocation failure, free previously allocated cache lines
+      free(cache->lines);
+      // Memory allocation failure, free previously allocated cache structure
+      free(cache);
+      // Memory allocation failure
       return NULL;
     }
   }
-
+  // return the cache structure
   return cache;
 }
 
 void cashier_release(struct cashier *cache) {
+  // Check for NULL pointer
   if (cache == NULL) {
+    // NULL pointer,
     return;
   }
 
-  // Write back dirty cache lines before releasing resources
-  for (uint64_t i = 0; i < cache->config.lines; i++) {
-    if (cache->lines[i].valid && cache->lines[i].dirty) {
-      before_eviction(i >> cache->index_bits, &cache->lines[i]);
+  // consider the sets with the number of cache lines
+  for (uint64_t i = 0; i < cache->config.ways; i++) {
+    // consider the cache lines
+    for (uint64_t j = 0; j < (cache->config.lines / cache->config.ways); j++) {
+      // if the cache line is valid, free the allocated memory for the cache line data
+      if (cache->lines[(i * (cache->config.lines / cache->config.ways)) + j].valid) {
+        // Free the allocated memory for the cache line data
+        free(cache->lines[(i * (cache->config.lines / cache->config.ways)) + j].data);
+      }
+      // if the cache line is not valid, firstly write back to memory
+      else {
+        // if the cache line is dirty, write back to memory
+        if (cache->lines[(i * (cache->config.lines / cache->config.ways)) + j].dirty) {
+          // write back to memory
+          before_eviction((i * (cache->config.lines / cache->config.ways)) + j, &cache->lines[(i * (cache->config.lines / cache->config.ways)) + j]);
+        }
+      }
     }
   }
-
-  // Free the allocated memory for data and cache lines
-  for (uint64_t i = 0; i < cache->config.lines; i++) {
-    free(cache->lines[i].data);
-  }
+  // Free the allocated memory for cache lines
   free(cache->lines);
 
   // Free the allocated memory for the cache structure
@@ -120,7 +122,9 @@ void cashier_release(struct cashier *cache) {
 }
 
 bool cashier_read(struct cashier *cache, uint64_t addr, uint8_t *byte) {
+  // Check for NULL pointer
   if (cache == NULL) {
+    // NULL pointer
     return false;
   }
 
